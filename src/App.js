c702e4efa50d91f65eacec622f62c0aa7920a90d
Amal Ben Kilani <eng.ben.kilani.amal@gmail.com>
2022-11-12 12:36:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import {Map,GoogleApiWrapper} from 'google-maps-react'
+import { Component } from 'react';
 
-function App() {
+class MapContainer extends Component {
+render ()  {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Map google={this.props.google} style={{width:"100%" , height:"100%"}} zoom={10} initialCenter={{lat:35.829300 , lng:10.640630}} />
     </div>
   );
 }
+}
 
-export default App;
+export default GoogleApiWrapper({apiKey:"AIzaSyD4xU8mDdAyKaJOPtrok7jpWbHVS3g3yKg"})(MapContainer);
